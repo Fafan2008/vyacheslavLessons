5dd9e516f981fa4ee7a8c79838cb974d49822d59
ivanov <12345678>
2020-10-22 21:18:18 +0300

    Added some tests of using filters and tasks in main app.

--- src/main/java/WebToDoList/Models/Task/Task.java
+++ src/main/java/WebToDoList/Models/Task/Task.java
@@ -7,16 +7,20 @@ public class Task implements ITask {
     public String userID;
     public String name;
     public String description;
-    public Priority priority;
+    public Priority priority = Priority.UNDEFINED;
     public Date timeToComplete;
-    public Date created;
+    public Date created = new Date();
 
     @Override
     public boolean update(Update updates) {
-        this.name = updates.name;
-        this.description = updates.description;
-        this.priority = updates.priority;
-        this.timeToComplete = updates.timeToComplete;
+        if (updates.name != null)
+            this.name = updates.name;
+        if (updates.description != null)
+            this.description = updates.description;
+        if (updates.priority != null)
+            this.priority = updates.priority;
+        if (updates.timeToComplete != null)
+            this.timeToComplete = updates.timeToComplete;
         // I assumed that I should send a request to the remote database and receive a response based on which I can return a boolean result.
         return true;
     }
