c882b0c0b9c8d5b73389b6e96e8486fc31bf3cb1
ivanov <12345678>
2020-10-27 19:08:05 +0300

    Edit main app logic. When app start, in method run it waits enter command and after execute it.
    Add console Authenticate;
    Add console add Task;

--- src/main/java/WebToDoList/DataBase/IDb.java
+++ src/main/java/WebToDoList/DataBase/IDb.java
@@ -2,10 +2,13 @@ package WebToDoList.DataBase;
 
 import WebToDoList.Models.Task.ITask;
 import WebToDoList.Models.Task.Task;
+import WebToDoList.Models.User.User;
 
 import java.util.List;
 
 public interface IDb {
-    public ITask getTask(String id);
+    public User getUser(String id);
+    public Task getTask(String id);
+    public void addTask(String id, Task task);
     public List<Task> getTasks(Search filter);
 }
