919594edeefc385d1645672c6d59be7e2271fbbb
ivanov <12345678>
2021-02-18 03:38:32 +0300

    New Solution of old tasks I commit here because, i don't know how do it in right branches.

--- src/main/java/leetcode/invertBinaryTree/Solution.java
+++ src/main/java/leetcode/invertBinaryTree/Solution.java
@@ -1,4 +1,10 @@
 package leetcode.invertBinaryTree;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
 ///Blaba
 public class Solution {
     public static TreeNode invertTree(TreeNode root){
@@ -11,8 +17,28 @@ public class Solution {
         invertTree(root.right);
         return root;
     }
+    public static TreeNode invertTree2(TreeNode root){
+        if (root == null)
+            return null;
+        TreeNode newTree = new TreeNode();
+        Deque<TreeNode> list = new ArrayDeque<>();
+        list.add(root);
+        while (!list.isEmpty()) {
+            TreeNode current = list.poll();
+            if(current!=null) {
+                TreeNode tmp = current.left;
+                current.left = current.right;
+                current.right = tmp;
+                if(current.left != null)
+                    list.add(current.left);
+                if(current.right != null)
+                    list.add(current.right);
+            }
+        }
+        return root;
+    }
     public static void main(String[] args){
-        TreeNode root;
+        TreeNode root1;
         TreeNode l1;
         TreeNode r1;
 
@@ -21,7 +47,25 @@ public class Solution {
 
         r1 = new TreeNode(3);
         l1 = new TreeNode(2, l2, r2);
-        root = new TreeNode(1, l1, r1);
-        TreeNode res = invertTree(root);
+        root1 = new TreeNode(1, l1, r1);
+        TreeNode res1 = invertTree(root1);
+
+        TreeNode root2;
+        TreeNode l11;
+        TreeNode r11;
+
+        TreeNode l22 = new TreeNode(4);
+        TreeNode r22 = new TreeNode(5);
+
+        r11 = new TreeNode(3);
+        l11 = new TreeNode(2, l22, r22);
+        root2 = new TreeNode(1, l11, r11);
+        TreeNode res2 = invertTree2(root2);
+
+        TreeNode root3;
+        TreeNode l111 = null;
+        TreeNode r111 = new TreeNode(2);
+        root3 = new TreeNode(1, l111, r111);
+        TreeNode res3 = invertTree2(root3);
     }
 }
--- src/main/java/leetcode/sumOfLeftLeaves/Solution.java
+++ src/main/java/leetcode/sumOfLeftLeaves/Solution.java
@@ -1,5 +1,9 @@
 package leetcode.sumOfLeftLeaves;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
 public class Solution {
     public static int sumOfLeftLeaves(TreeNode root){
         if (root == null)
@@ -8,6 +12,26 @@ public class Solution {
             return root.left.val + sumOfLeftLeaves(root.right);
         else
             return sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
+    }
+    //Важно! Другой способ решения. Жрет много памяти. Что можно сделать?
+    public static int sumOfLeftLeaves2(TreeNode root){
+        int sum = 0;
+        List<TreeNode> stack = new ArrayList<TreeNode>();
+        if (root != null)
+            stack.add(root);
+        while (!stack.isEmpty())
+        {
+            TreeNode currentTree = stack.get(stack.size()-1);
+            stack.remove(stack.size()-1);
+            if (currentTree.left!=null && currentTree.left.left == null && currentTree.left.right == null) {
+                sum += currentTree.left.val;
+            }
+            if(currentTree.left != null)
+                stack.add(currentTree.left);
+            if(currentTree.right != null)
+                stack.add(currentTree.right);
+        }
+        return sum;
     }
     public static void main(String[] args){
         TreeNode root;
@@ -20,6 +44,7 @@ public class Solution {
         r1 = new TreeNode(3);
         l1 = new TreeNode(2, l2, r2);
         root = new TreeNode(1, l1, r1);
-        int res = sumOfLeftLeaves(root);
+        int res1 = sumOfLeftLeaves(root);
+        int res2 = sumOfLeftLeaves2(root);
     }
 }
