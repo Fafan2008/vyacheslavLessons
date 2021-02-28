53ec23d22271306476eb3fc72e1705cde55b073d
ivanov <12345678>
2021-02-28 20:20:53 +0300

    Solution of Implement strStr() https://leetcode.com/problems/implement-strstr/

---
+++ src/main/java/leetcode/implementstrStr/Solution.java
@@ -0,0 +1,24 @@
+package leetcode.implementstrStr;
+
+public class Solution {
+    public static int strStr(String haystack, String needle){
+        if (needle.isEmpty()) return 0;
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            for (int j = 0; j < needle.length() && haystack.charAt(i + j) == needle.charAt(j); j++)
+                if (j == needle.length() - 1) return i;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int i = 1;
+        {
+            String str1 = "mississippi";
+            String str2 = "issip";
+            int result = strStr(str1, str2);
+            System.out.print("Test " + i + " return: ");
+            System.out.println(result);
+            ++i;
+        }
+    }
+}
